a2a9c6b290b891003f1d2ce5c893c0d244761a70
nicoleta-tuturuga <nicoleta.tuturuga@yahoo.com>
2020-11-09 15:29:25 +0200

    create DisplayRepoList component for screen 2

---
+++ src/components/screen2/DisplayRepoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const displayRepoList = () => {
+
+  return(
+    <div>
+      <h1>Screen 2</h1>
+      <p>username x</p>
+      <ul>
+        <li>the list elements goes here</li>
+      </ul>
+    </div>
+  )
+}
+
+export default displayRepoList;
